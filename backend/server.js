abccc6062e4af750761f723b3ef83b482990c470
Dan-tk <thiongodan405@gmail.com>
2024-06-15 10:22:44 +0300

    added models

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const cors=require('cors')
+const multer=require('multer')
+const path=require("path")
+const cookieParser=require('cookie-parser')
+
+//connect to database
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database is connected successfully!")
+
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+dotenv.config()
+
+app.listen(process.env.PORT,()=>{
+    connectDB()
+    console.log("app is running on port "+process.env.PORT)
+})
