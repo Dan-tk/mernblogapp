b590f8bb2610f174b58eeba7ba14f36dafd6445c
Dan-tk <thiongodan405@gmail.com>
2024-06-18 12:42:25 +0300

    url 2

--- frontend/url.js
+++ frontend/url.js
@@ -1,5 +1,5 @@
-export const URL="https://mernblogapp-5.onrender.com:3000"
-export const IMAGE="https://mernblogapp-5.onrender.com:3000/images/"
+export const URL="https://mernblogapp-5.onrender.com"
+export const IMAGE="https://mernblogapp-5.onrender.com/images/"
 
 //export const URL="http://localhost:3000"
 //export const IMAGE="http://localhost:3000/images/"
